c0c0cc0222f19610a480f4d2d9bcdfa36e58c7c2
René Fehlow <rene.fehlow@web.de>
2022-07-18 17:29:03 +0200

    Cleanup

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,7 +1,7 @@
-import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path'
@@ -36,42 +36,23 @@ export class InfrastructureStack extends Stack {
       runtime: Runtime.NODEJS_14_X,
     }
 
-    // Create a Lambda function for each of the CRUD operations
+    // Create Lambda functions
     const getOneLambda = new NodejsFunction(this, 'getOneItemFunction', {
       entry: join(__dirname, 'lambdas', 'get-one.ts'),
       ...nodeJsFunctionProps,
     });
-    // const getAllLambda = new NodejsFunction(this, 'getAllItemsFunction', {
-    //   entry: join(__dirname, 'lambdas', 'get-all.ts'),
-    //   ...nodeJsFunctionProps,
-    // });
     const createOneLambda = new NodejsFunction(this, 'createItemFunction', {
       entry: join(__dirname, 'lambdas', 'create.ts'),
       ...nodeJsFunctionProps,
     });
-    // const updateOneLambda = new NodejsFunction(this, 'updateItemFunction', {
-    //   entry: join(__dirname, 'lambdas', 'update-one.ts'),
-    //   ...nodeJsFunctionProps,
-    // });
-    // const deleteOneLambda = new NodejsFunction(this, 'deleteItemFunction', {
-    //   entry: join(__dirname, 'lambdas', 'delete-one.ts'),
-    //   ...nodeJsFunctionProps,
-    // });
 
     // Grant the Lambda function read access to the DynamoDB table
-    // dynamoTable.grantReadWriteData(getAllLambda);
     dynamoTable.grantReadWriteData(getOneLambda);
     dynamoTable.grantReadWriteData(createOneLambda);
-    // dynamoTable.grantReadWriteData(updateOneLambda);
-    // dynamoTable.grantReadWriteData(deleteOneLambda);
 
     // Integrate the Lambda functions with the API Gateway resource
-    // const getAllIntegration = new LambdaIntegration(getAllLambda);
     const createOneIntegration = new LambdaIntegration(createOneLambda);
     const getOneIntegration = new LambdaIntegration(getOneLambda);
-    // const updateOneIntegration = new LambdaIntegration(updateOneLambda);
-    // const deleteOneIntegration = new LambdaIntegration(deleteOneLambda);
-
 
     // Create an API Gateway resource for each of the CRUD operations
     const api = new RestApi(this, 'itemsApi', {
@@ -80,42 +61,8 @@ export class InfrastructureStack extends Stack {
 
     const items = api.root.addResource('items');
     items.addMethod('POST', createOneIntegration);
-    addCorsOptions(items);
 
     const singleItem = items.addResource('{id}');
     singleItem.addMethod('GET', getOneIntegration);
-    addCorsOptions(singleItem);
   }
 }
-
-export function addCorsOptions(apiResource: IResource) {
-  apiResource.addMethod('OPTIONS', new MockIntegration({
-    integrationResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
-        'method.response.header.Access-Control-Allow-Credentials': "'false'",
-        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
-      },
-    }],
-    passthroughBehavior: PassthroughBehavior.NEVER,
-    requestTemplates: {
-      "application/json": "{\"statusCode\": 200}"
-    },
-  }), {
-    methodResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-      },
-    }]
-  })
-}
-
-const app = new App();
-new InfrastructureStack(app, 'ApiLambdaCrudDynamoDBExample');
-app.synth();
