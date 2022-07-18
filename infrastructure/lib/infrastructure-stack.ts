532ff215fa661aa198d7e448622ccb9027f7a7fc
René Fehlow <rene.fehlow@web.de>
2022-07-18 17:14:48 +0200

    Works on my machine

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,6 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { App, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path'
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +12,110 @@ export class InfrastructureStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const dynamoTable = new Table(this, 'items', {
+      partitionKey: {
+        name: 'itemId',
+        type: AttributeType.STRING
+      },
+      tableName: 'items',
+      //default removal policy is RETAIN, which means that cdk destroy will not attempt to delete the new table
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
     });
+
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+        ],
+      },
+      depsLockFilePath: join(__dirname, 'lambdas', 'package-lock.json'),
+      environment: {
+        PRIMARY_KEY: 'itemId',
+        TABLE_NAME: dynamoTable.tableName,
+      },
+      runtime: Runtime.NODEJS_14_X,
+    }
+
+    // Create a Lambda function for each of the CRUD operations
+    const getOneLambda = new NodejsFunction(this, 'getOneItemFunction', {
+      entry: join(__dirname, 'lambdas', 'get-one.ts'),
+      ...nodeJsFunctionProps,
+    });
+    // const getAllLambda = new NodejsFunction(this, 'getAllItemsFunction', {
+    //   entry: join(__dirname, 'lambdas', 'get-all.ts'),
+    //   ...nodeJsFunctionProps,
+    // });
+    const createOneLambda = new NodejsFunction(this, 'createItemFunction', {
+      entry: join(__dirname, 'lambdas', 'create.ts'),
+      ...nodeJsFunctionProps,
+    });
+    // const updateOneLambda = new NodejsFunction(this, 'updateItemFunction', {
+    //   entry: join(__dirname, 'lambdas', 'update-one.ts'),
+    //   ...nodeJsFunctionProps,
+    // });
+    // const deleteOneLambda = new NodejsFunction(this, 'deleteItemFunction', {
+    //   entry: join(__dirname, 'lambdas', 'delete-one.ts'),
+    //   ...nodeJsFunctionProps,
+    // });
+
+    // Grant the Lambda function read access to the DynamoDB table
+    // dynamoTable.grantReadWriteData(getAllLambda);
+    dynamoTable.grantReadWriteData(getOneLambda);
+    dynamoTable.grantReadWriteData(createOneLambda);
+    // dynamoTable.grantReadWriteData(updateOneLambda);
+    // dynamoTable.grantReadWriteData(deleteOneLambda);
+
+    // Integrate the Lambda functions with the API Gateway resource
+    // const getAllIntegration = new LambdaIntegration(getAllLambda);
+    const createOneIntegration = new LambdaIntegration(createOneLambda);
+    const getOneIntegration = new LambdaIntegration(getOneLambda);
+    // const updateOneIntegration = new LambdaIntegration(updateOneLambda);
+    // const deleteOneIntegration = new LambdaIntegration(deleteOneLambda);
+
+
+    // Create an API Gateway resource for each of the CRUD operations
+    const api = new RestApi(this, 'itemsApi', {
+      restApiName: 'Items Service'
+    });
+
+    const items = api.root.addResource('items');
+    items.addMethod('POST', createOneIntegration);
+    addCorsOptions(items);
+
+    const singleItem = items.addResource('{id}');
+    singleItem.addMethod('GET', getOneIntegration);
+    addCorsOptions(singleItem);
   }
 }
+
+export function addCorsOptions(apiResource: IResource) {
+  apiResource.addMethod('OPTIONS', new MockIntegration({
+    integrationResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+        'method.response.header.Access-Control-Allow-Origin': "'*'",
+        'method.response.header.Access-Control-Allow-Credentials': "'false'",
+        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+      },
+    }],
+    passthroughBehavior: PassthroughBehavior.NEVER,
+    requestTemplates: {
+      "application/json": "{\"statusCode\": 200}"
+    },
+  }), {
+    methodResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': true,
+        'method.response.header.Access-Control-Allow-Methods': true,
+        'method.response.header.Access-Control-Allow-Credentials': true,
+        'method.response.header.Access-Control-Allow-Origin': true,
+      },
+    }]
+  })
+}
+
+const app = new App();
+new InfrastructureStack(app, 'ApiLambdaCrudDynamoDBExample');
+app.synth();
