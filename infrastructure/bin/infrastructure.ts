09fa9f0120280b153044ade68b1c6652de5dfbbb
Sven Ulrich <sven.ulrich@datev.de>
2022-07-20 08:39:10 +0200

    trying another way of referencing lambda

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -5,5 +5,5 @@ import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
 new InfrastructureStack(app, 'CodingFestival-InfrastructureAsCode', {  
-  env: { account: '947992995263', region: 'eu-central-1' }
+  env: { account: '807721111563', region: 'eu-central-1' }
 });
